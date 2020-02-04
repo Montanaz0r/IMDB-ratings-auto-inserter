f84ae52883e1d86fbb34776e725488d9a83758c5
Montanaz0r <52318290+Montanaz0r@users.noreply.github.com>
2020-02-04 13:44:51 +0100

    creatings init files

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from selenium import webdriver
+
+# loading file with movie ratings as DataFrame from file
+df = pd.read_pickle('ratings.pkl')
+
+# path to webdriver
+wb_path = 'chromedriver.exe'
+driver = webdriver.Chrome(executable_path=wb_path)
